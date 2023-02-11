bfee89357757891994e7377405068ea2c0ce025b
Pedro Fonseca <pedrinhopedro01@gmail.com>
2023-02-11 13:27:53 -0300

    feat: implementa o codigo pra buscar o pokemon

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,39 @@
 const pokemonName = document.querySelector('.pokemon__name');
 const pokemonID = document.querySelector('.pokemon__id');
+const pokemonIMG = document.querySelector('.pokemon__img');
+const pokemonHP = document.querySelector('.pokemon__hp');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.input__search');
 
 const fetchPokemon = async (pokemon) => {
-  const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+  const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`)
   const data = await APIresponse.json()
   return data
 }
 
 renderPokemon = async (pokemon) => {
+  pokemonName.innerHTML = 'pokemon'
+  pokemonID.innerHTML = ''
+
   const data = await fetchPokemon(pokemon)
-  console.log(data.name)
-  console.log(data.id)
-  pokemonName.innerHTML = data.name
-  pokemonID.innerHTML = data.id
+
+  if (data) {
+    pokemonIMG.style.display = 'block'
+    pokemonName.innerHTML = data.name
+    pokemonID.innerHTML = data.id
+    pokemonIMG.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+    console.log(pokemonHP.innerHTML = data['stats']['stat'])
+    input.value = ''
+  } else {
+    pokemonIMG.style.display = 'none'
+    pokemonName.innerHTML = 'Não encontrado :c'
+    // pokemonID.innerHTML = data.id
+  }
 }
 
-renderPokemon('13')
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  renderPokemon(input.value)
+})
